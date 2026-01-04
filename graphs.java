db8553785bd7c03fb5a6ef706345350628a81a96
Ananya Kharbanda <ananyakharbanda28@gmail.com>
2026-01-04 22:01:41 +0800

    wrote bipartite dfs

--- graphs.java
+++ graphs.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
+
 class Stack {
     public ArrayList<Integer> data;
 
@@ -18,7 +19,7 @@ class Stack {
     
     public int pop() {
         int toreturn = this.data.get(this.data.size() - 1);
-        this.data.remove(this.data.get(this.data.size() - 1));
+        this.data.remove(this.data.size() - 1);
         return toreturn;
     }
 }
@@ -34,6 +35,7 @@ class Queue {
     public void push(int i) {
         this.data.add(i);
     }
+
     public boolean isEmpty() {
         return this.data.size() == 0;   
     }
@@ -47,10 +49,18 @@ class Queue {
 
 
 class Graph {
-    public List<List<Integer>> g;
-        
+
+    private List<List<Integer>> g;
+    private int[] colours;
+    private boolean isBp;
+
+    public static final int WHITE = 0;
+    public static final int RED = 1;
+    public static final int BLUE = 2;
+    
     public Graph(List<List<Integer>> g) {
         this.g = g;
+        isBp = true;
     }
     
     public void traverseDFS(int start) {
@@ -70,19 +80,80 @@ class Graph {
             }
         }
     }
+    
+    public void traverseBFS(int start) {
+        Queue q = new Queue();
+        List<Integer> visited = new ArrayList<>();
+        
+        q.push(start);
+        while(!q.isEmpty()) {
+            int curr = q.pop();
+            if (!visited.contains(curr)) {
+                visited.add(curr);
+                System.out.println(curr);
+                List<Integer> neighbours = g.get(curr - 1);
+                for (int i : neighbours) {
+                    q.push(i);
+                }
+            }
+        }
+    }
+    
+    public boolean checkBipartite(int start, int startColour, int n) { 
+        colours = new int[n];
+        Arrays.fill(colours, WHITE);
+        bipartiteDFS(start, startColour);
+        return isBp;
+    }
+
+    public void bipartiteDFS(int curr, int colour) {
+
+        if (colours[curr - 1] != WHITE) {
+            if (colours[curr - 1] != colour) {
+                this.isBp = false;
+            }
+            return;
+        }
+
+        colours[curr - 1] = colour;
+    
+        for (int v : g.get(curr - 1)) {
+            if (colour  == BLUE) {
+                bipartiteDFS(v, RED);
+            } else if(colour == RED){
+                bipartiteDFS(v, BLUE);
+            }
+        }
+    }
 }
 
 
 class GraphMain {
     public static void main(String[] args) {
+
         List<List<Integer>> adjacencyList = new ArrayList<>();
+        List<List<Integer>> adjacencyList2 = new ArrayList<>();
+
         adjacencyList.add(new ArrayList<Integer>(Arrays.asList(2,3)));
         adjacencyList.add(new ArrayList<Integer>(Arrays.asList(1,3,4,5)));
         adjacencyList.add(new ArrayList<Integer>(Arrays.asList(1,2)));
         adjacencyList.add(new ArrayList<Integer>(Arrays.asList(2,5)));
         adjacencyList.add(new ArrayList<Integer>(Arrays.asList(2,4)));
+
+
+        adjacencyList2.add(new ArrayList<Integer>(Arrays.asList(2,4)));
+        adjacencyList2.add(new ArrayList<Integer>(Arrays.asList(1,3)));
+        adjacencyList2.add(new ArrayList<Integer>(Arrays.asList(2,4)));
+        adjacencyList2.add(new ArrayList<Integer>(Arrays.asList(1,3)));
+
         Graph g = new Graph(adjacencyList);
+        Graph g2 = new Graph(adjacencyList2);
+
         g.traverseDFS(1);
-        
+
+        g.traverseBFS(1);
+
+        System.out.println(g.checkBipartite(1, Graph.BLUE, 5)); 
+        System.out.println(g2.checkBipartite(1, Graph.BLUE, 4)); 
     }
 }
