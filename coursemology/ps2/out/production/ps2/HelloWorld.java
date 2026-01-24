565978bd6edff4f759c0aaa1c38bd5efbc150eba
Ananya Kharbanda <ananyakharbanda28@gmail.com>
2026-01-24 14:09:46 +0800

    started kruskals

---
+++ coursemology/ps2/out/production/ps2/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello CS2040S!");
+        System.out.println("Name:");
+        System.out.println("Ananya");
+
+        System.out.println("Favourite algorithm / joke:");
+        System.out.println("Binary Search because cutting the problem in half never gets old.");
+
+        System.out.println("I am a student interested in problem solving and algorithms.");
+        System.out.println("I enjoy learning how theoretical ideas translate into efficient code.");
+        System.out.println("I am taking CS2040S to strengthen my fundamentals in data structures and algorithms.");
+
+        System.out.println("3125");
+        System.out.println("argA to the power of argB");
+    }
+}
