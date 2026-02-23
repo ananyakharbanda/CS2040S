ec32b49017e1b776e2a6b8758039100152cf1088
Ananya Kharbanda <ananyakharbanda28@gmail.com>
2026-02-23 18:57:14 +0800

    done with optional prac 2

--- optionalPrac/WiFi/src/WiFi.java
+++ optionalPrac/WiFi/src/WiFi.java
@@ -6,13 +6,40 @@ class WiFi {
      * Implement your solution here
      */
     public static double computeDistance(int[] houses, int numOfAccessPoints) {
-        return 0.0;
+        Arrays.sort(houses);
+        double low = 0.0;
+        double high = houses[houses.length - 1] - houses[0];
+        
+        while (high - low > 1e-6) {
+            double mid = low + (high - low) / 2.0;
+            
+            if (coverable(houses, numOfAccessPoints, mid)) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+
+        return low;
     }
 
     /**
      * Implement your solution here
      */
     public static boolean coverable(int[] houses, int numOfAccessPoints, double distance) {
-        return false;
+       
+        int i = 0;
+
+        while (i < houses.length && numOfAccessPoints > 0) {
+            double range = houses[i] + 2 * distance;
+           
+            while (i < houses.length && houses[i] <= range) {
+                i++;
+            }
+            
+            numOfAccessPoints--; 
+        }
+         
+        return i == houses.length;
     }
 }
